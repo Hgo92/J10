52776ce5143cfb7729ecd7ad111c7ad44441dd0c
Hgo92 <hugo_monier@yahoo.fr>
2025-11-09 17:08:49 +0100

    MAJ finale script

--- script.mjs
+++ script.mjs
@@ -12,6 +12,9 @@ async function loadPokedex() {
 
 loadPokedex()
 
+
+//Etape bonus : j'ai créé un Pokedex qui affiche les infos du Pokemon demandé ! 
+
 const boutonChoix = document.getElementById("buttonPoke");
 const choix = document.getElementById("choixPoke");
 
@@ -29,13 +32,11 @@ let faiblessesCarte = document.getElementById("faiblesses");
 let backCard = document.getElementById("back-card")
 
 carte.style.display="none";
+backCard.style.display="block";
 
 function createCard(name) {
-  carte.backgroundImage = "url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/176622e8-bf4c-4d60-9a64-b75cff245c16/d3inhyi-28e16cb6-9d37-4a6e-bdc6-0d7fcf0af574.pnghttps://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/176622e8-bf4c-4d60-9a64-b75cff245c16/d3inhyi-28e16cb6-9d37-4a6e-bdc6-0d7fcf0af574.png/v1/fit/w_426,h_613,q_70,strp/pokemon_tcg_colorless_texture_by_ilkcmp_d3inhyi-375w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NjEzIiwicGF0aCI6Ii9mLzE3NjYyMmU4LWJmNGMtNGQ2MC05YTY0LWI3NWNmZjI0NWMxNi9kM2luaHlpLTI4ZTE2Y2I2LTlkMzctNGE2ZS1iZGM2LTBkN2ZjZjBhZjU3NC5wbmciLCJ3aWR0aCI6Ijw9NDI2In1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.yjZnG5s6Tu1I5WrXGhd1j2jDkzu_veGT1JsOsfGk7nw')";
     let search = pokedex.pokemon.find(n => n.name === name)
-    if (search) {
-      
-    
+    if (search && search != undefined) {
       let faiblesses = search.weaknesses;
       let faiblessesText = "";
 
@@ -87,8 +88,10 @@ function createCard(name) {
     boxImage.style.backgroundColor = "#94bc4a";
   }  else if (pokeTypes[0] === "Dark") {
     boxImage.style.backgroundColor = "#736c75"
+    carte.backgroundImage = "url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/176622e8-bf4c-4d60-9a64-b75cff245c16/d3inhyi-28e16cb6-9d37-4a6e-bdc6-0d7fcf0af574.png/v1/fit/w_426,h_613,q_70,strp/pokemon_tcg_colorless_texture_by_ilkcmp_d3inhyi-375w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NjEzIiwicGF0aCI6Ii9mLzE3NjYyMmU4LWJmNGMtNGQ2MC05YTY0LWI3NWNmZjI0NWMxNi9kM2luaHlpLTI4ZTE2Y2I2LTlkMzctNGE2ZS1iZGM2LTBkN2ZjZjBhZjU3NC5wbmciLCJ3aWR0aCI6Ijw9NDI2In1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.yjZnG5s6Tu1I5WrXGhd1j2jDkzu_veGT1JsOsfGk7nw')";
   }  else if (pokeTypes[0] === "Dragon") {
     boxImage.style.backgroundColor = "#6a7baf"
+    carte.backgroundImage = "url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/176622e8-bf4c-4d60-9a64-b75cff245c16/d3inhyi-28e16cb6-9d37-4a6e-bdc6-0d7fcf0af574.png/v1/fit/w_426,h_613,q_70,strp/pokemon_tcg_colorless_texture_by_ilkcmp_d3inhyi-375w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NjEzIiwicGF0aCI6Ii9mLzE3NjYyMmU4LWJmNGMtNGQ2MC05YTY0LWI3NWNmZjI0NWMxNi9kM2luaHlpLTI4ZTE2Y2I2LTlkMzctNGE2ZS1iZGM2LTBkN2ZjZjBhZjU3NC5wbmciLCJ3aWR0aCI6Ijw9NDI2In1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.yjZnG5s6Tu1I5WrXGhd1j2jDkzu_veGT1JsOsfGk7nw')";
   }  else if (pokeTypes[0] === "Electric") {
     boxImage.style.backgroundColor = "#e5c531";
     carte.style.backgroundImage = "url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/81ccfdb7-ac1d-423f-a276-af63abae5ba2/d5oun1x-6975d2e1-093f-4f3f-88f4-21ea09a01e93.png/v1/fit/w_431,h_614,q_70,strp/lightning__bw__texture_by_pkmncardmaker264_d5oun1x-375w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NjE0IiwicGF0aCI6Ii9mLzgxY2NmZGI3LWFjMWQtNDIzZi1hMjc2LWFmNjNhYmFlNWJhMi9kNW91bjF4LTY5NzVkMmUxLTA5M2YtNGYzZi04OGY0LTIxZWEwOWEwMWU5My5wbmciLCJ3aWR0aCI6Ijw9NDMxIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.PeD5IO0STKsVngGHHt3msoKiNO15VImGjQZB8di1Tp0')";
@@ -100,8 +103,10 @@ function createCard(name) {
     carte.style.backgroundImage = "url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/f144fa15-4001-4f05-83d9-73ad837f6bc4/d4wcj31-53a8338d-c2b1-4a00-ad15-aaa314cb9f6b.png/v1/fit/w_425,h_612,q_70,strp/classic_pkmn_tcg_texture_pack_by_flamingclaw_d4wcj31-375w-2x.jpg')"
   }  else if (pokeTypes[0] === "Flying") {
     boxImage.style.backgroundColor = "#7da6de"
+    carte.backgroundImage = "url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/176622e8-bf4c-4d60-9a64-b75cff245c16/d3inhyi-28e16cb6-9d37-4a6e-bdc6-0d7fcf0af574.png/v1/fit/w_426,h_613,q_70,strp/pokemon_tcg_colorless_texture_by_ilkcmp_d3inhyi-375w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NjEzIiwicGF0aCI6Ii9mLzE3NjYyMmU4LWJmNGMtNGQ2MC05YTY0LWI3NWNmZjI0NWMxNi9kM2luaHlpLTI4ZTE2Y2I2LTlkMzctNGE2ZS1iZGM2LTBkN2ZjZjBhZjU3NC5wbmciLCJ3aWR0aCI6Ijw9NDI2In1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.yjZnG5s6Tu1I5WrXGhd1j2jDkzu_veGT1JsOsfGk7nw')";
   }  else if (pokeTypes[0] === "Ghost") {
     boxImage.style.backgroundColor = "#846ab6"
+    carte.backgroundImage = "url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/176622e8-bf4c-4d60-9a64-b75cff245c16/d3inhyi-28e16cb6-9d37-4a6e-bdc6-0d7fcf0af574.png/v1/fit/w_426,h_613,q_70,strp/pokemon_tcg_colorless_texture_by_ilkcmp_d3inhyi-375w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NjEzIiwicGF0aCI6Ii9mLzE3NjYyMmU4LWJmNGMtNGQ2MC05YTY0LWI3NWNmZjI0NWMxNi9kM2luaHlpLTI4ZTE2Y2I2LTlkMzctNGE2ZS1iZGM2LTBkN2ZjZjBhZjU3NC5wbmciLCJ3aWR0aCI6Ijw9NDI2In1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.yjZnG5s6Tu1I5WrXGhd1j2jDkzu_veGT1JsOsfGk7nw')";
   }  else if (pokeTypes[0] === "Ground") {
     boxImage.style.style.backgroundColor = "#cc9f4f "
     carte.style.backgroundImage = "url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/f144fa15-4001-4f05-83d9-73ad837f6bc4/d4wcj31-53a8338d-c2b1-4a00-ad15-aaa314cb9f6b.png/v1/fit/w_425,h_612,q_70,strp/classic_pkmn_tcg_texture_pack_by_flamingclaw_d4wcj31-375w-2x.jpg')"
@@ -110,27 +115,35 @@ function createCard(name) {
     carte.style.backgroundImage = "url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/176622e8-bf4c-4d60-9a64-b75cff245c16/d6ada67-356f47fc-c8fe-49b7-b59a-313219d7022c.png/v1/fit/w_425,h_611/pokemon_tcg_ice_texture_by_ilkcmp_d6ada67-375w-2x.png')"
   }  else if (pokeTypes[0] === "Normal") {
     boxImage.style.backgroundColor = "#aab09f "
+    carte.backgroundImage = "url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/176622e8-bf4c-4d60-9a64-b75cff245c16/d3inhyi-28e16cb6-9d37-4a6e-bdc6-0d7fcf0af574.png/v1/fit/w_426,h_613,q_70,strp/pokemon_tcg_colorless_texture_by_ilkcmp_d3inhyi-375w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NjEzIiwicGF0aCI6Ii9mLzE3NjYyMmU4LWJmNGMtNGQ2MC05YTY0LWI3NWNmZjI0NWMxNi9kM2luaHlpLTI4ZTE2Y2I2LTlkMzctNGE2ZS1iZGM2LTBkN2ZjZjBhZjU3NC5wbmciLCJ3aWR0aCI6Ijw9NDI2In1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.yjZnG5s6Tu1I5WrXGhd1j2jDkzu_veGT1JsOsfGk7nw')";
   }  else if (pokeTypes[0] === "Poison") {
     boxImage.style.backgroundColor = "#b468b7"
+    carte.backgroundImage = "url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/176622e8-bf4c-4d60-9a64-b75cff245c16/d3inhyi-28e16cb6-9d37-4a6e-bdc6-0d7fcf0af574.png/v1/fit/w_426,h_613,q_70,strp/pokemon_tcg_colorless_texture_by_ilkcmp_d3inhyi-375w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NjEzIiwicGF0aCI6Ii9mLzE3NjYyMmU4LWJmNGMtNGQ2MC05YTY0LWI3NWNmZjI0NWMxNi9kM2luaHlpLTI4ZTE2Y2I2LTlkMzctNGE2ZS1iZGM2LTBkN2ZjZjBhZjU3NC5wbmciLCJ3aWR0aCI6Ijw9NDI2In1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.yjZnG5s6Tu1I5WrXGhd1j2jDkzu_veGT1JsOsfGk7nw')";
   }  else if (pokeTypes[0] === "Psychic") {
     boxImage.style.backgroundColor = "#e5709b"
+    carte.backgroundImage = "url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/176622e8-bf4c-4d60-9a64-b75cff245c16/d3inhyi-28e16cb6-9d37-4a6e-bdc6-0d7fcf0af574.png/v1/fit/w_426,h_613,q_70,strp/pokemon_tcg_colorless_texture_by_ilkcmp_d3inhyi-375w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NjEzIiwicGF0aCI6Ii9mLzE3NjYyMmU4LWJmNGMtNGQ2MC05YTY0LWI3NWNmZjI0NWMxNi9kM2luaHlpLTI4ZTE2Y2I2LTlkMzctNGE2ZS1iZGM2LTBkN2ZjZjBhZjU3NC5wbmciLCJ3aWR0aCI6Ijw9NDI2In1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.yjZnG5s6Tu1I5WrXGhd1j2jDkzu_veGT1JsOsfGk7nw')";
   }  else if (pokeTypes[0] === "Rock") {
     carte.style.backgroundImage = "url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f23c48c4-218f-4490-a23d-551fbf5b3603/d5axd7v-f73ff5e5-9b71-4353-9856-5391cf0e2272.png/v1/fit/w_420,h_590,q_70,strp/metal_texture_by_shiningbill_d5axd7v-375w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NTkwIiwicGF0aCI6Ii9mL2YyM2M0OGM0LTIxOGYtNDQ5MC1hMjNkLTU1MWZiZjViMzYwMy9kNWF4ZDd2LWY3M2ZmNWU1LTliNzEtNDM1My05ODU2LTUzOTFjZjBlMjI3Mi5wbmciLCJ3aWR0aCI6Ijw9NDIwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.d0qutDhIdaBZjse8bX3KOrvCXeD1WJFwbVf33SQkMPA')"
     boxImage.style.backgroundColor = "#b2a061 "
   }  else if (pokeTypes[0] === "Steel") {
     boxImage.style.backgroundColor = "#89a1b0 "
+    carte.backgroundImage = "url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f23c48c4-218f-4490-a23d-551fbf5b3603/d5axd7v-f73ff5e5-9b71-4353-9856-5391cf0e2272.png/v1/fit/w_420,h_590,q_70,strp/metal_texture_by_shiningbill_d5axd7v-375w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NTkwIiwicGF0aCI6Ii9mL2YyM2M0OGM0LTIxOGYtNDQ5MC1hMjNkLTU1MWZiZjViMzYwMy9kNWF4ZDd2LWY3M2ZmNWU1LTliNzEtNDM1My05ODU2LTUzOTFjZjBlMjI3Mi5wbmciLCJ3aWR0aCI6Ijw9NDIwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.d0qutDhIdaBZjse8bX3KOrvCXeD1WJFwbVf33SQkMPA')";
   }  else if (pokeTypes[0] === "Water") {
     carte.style.backgroundImage = "url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/176622e8-bf4c-4d60-9a64-b75cff245c16/d3inhf4-271fd765-dfec-44fc-9985-3538b0bceb36.png/v1/fit/w_428,h_615,q_70,strp/pokemon_tcg_water_texture_rip_by_ilkcmp_d3inhf4-375w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NjE1IiwicGF0aCI6Ii9mLzE3NjYyMmU4LWJmNGMtNGQ2MC05YTY0LWI3NWNmZjI0NWMxNi9kM2luaGY0LTI3MWZkNzY1LWRmZWMtNDRmYy05OTg1LTM1MzhiMGJjZWIzNi5wbmciLCJ3aWR0aCI6Ijw9NDI4In1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.iI4lOGGSjqLxVgCF6idZIWsUVFIFGbhmmW7C45fXl3Q')";
     boxImage.style.backgroundColor = "#539ae2 "
-  }  
+  }
+  
+  backCard.style.display="none";
+  carte.style.display="grid";
 } }
 
 boutonChoix.addEventListener("click", () => {
-let nomPoke = choix.value;
-  createCard(nomPoke);
-choix.value = "";
-backCard.style.display="none"
-carte.style.display="grid";
+
+  if (choix.value) {
+    let nomPoke = choix.value;
+    createCard(nomPoke);
+    choix.value = "";
+}
 })
 
 
